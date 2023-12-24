cf276119b0c1d3e4b9e0d3f6a5019c8390c35457
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-12-24 21:02:52 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,64 +1,27 @@
-    for item in data:
-        print(f"Random Number: {item}")
-
-def main():
-    data = generate_random_data()
-if __name__ == "__main__":
-
-    return data
-
-
-    main()
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():def generate_random_data():
-
-
-        print(f"Random Number: {item}")
-    return data
-    main()
-    data = generate_random_data()
-
-if __name__ == "__main__":
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-
-    for item in data:        }
-public class RandomDataGenerator {
-    public static void main(String[] args) {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-}
-        int[] data = new int[10];
-    }
-
-
-import random
-    data = generate_random_data()
-    for item in data:
-
-    return data
 if __name__ == "__main__":
 def generate_random_data():
     main()
+    data = generate_random_data()
+
 def main():
+import random
+
+
+        print(f"Random Number: {item}")
 
     data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    return dataif __name__ == "__main__":
+    main()
+        print(f"Random Number: {item}")
 
-        print(f"Random Number: {item}")            data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
+def main():
+    for item in data:
+import random
 
-        }
-        int[] data = new int[10];
 
-public class RandomDataGenerator {
-        }
-    }
-        for (int item : data) {
-}
-            System.out.println("Random Number: " + item);
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+def generate_random_data():
+    data = generate_random_data()
